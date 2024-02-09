0b13d2f0d2a8d8b41227df2fa0be53791e8c8953
Isaac-Chow <99174054+Isaac-Chow@users.noreply.github.com>
2024-02-09 15:21:30 +0000

    theme

--- run.js
+++ run.js
@@ -5,7 +5,7 @@ function generate(){
         alert("Please input a number between 1 and 114")
     }else{
         t = []
-
+        
         while(c != 0){
             temp=l[Math.floor(Math.random()*l.length)]
             if(t.includes(temp)){   
@@ -15,8 +15,38 @@ function generate(){
                 c=c -1
             }
         }
-    
+        
         document.getElementById("gen").value = t
     }
-   
+    
+}
+
+function theme(){
+    localStorage.setItem("theme",document.getElementById("nymode").checked)
+    if(document.getElementById("nymode").checked){
+        document.body.style.backgroundColor = "red";
+        document.body.style.color = "gold";
+        document.getElementById("b").style.backgroundColor = "red";
+        document.getElementById("b").style.color = "gold";
+        document.getElementById("count").style.backgroundColor = "red";
+        document.getElementById("count").style.color = "gold";
+        document.getElementById("gen").style.backgroundColor = "red";
+        document.getElementById("gen").style.color = "gold";
+    } else {
+        document.body.style.backgroundColor = "white";
+        document.body.style.color = "black";
+        document.getElementById("b").style.backgroundColor = "white";
+        document.getElementById("b").style.color = "black";
+        document.getElementById("count").style.backgroundColor = "white";
+        document.getElementById("count").style.color = "black";
+        document.getElementById("gen").style.backgroundColor = "white";
+        document.getElementById("gen").style.color = "black";
+    }
+}
+
+function check(){
+    if(localStorage.getItem("theme")=="true"){
+        document.getElementById("nymode").checked = true
+        theme()
+    }
 }
